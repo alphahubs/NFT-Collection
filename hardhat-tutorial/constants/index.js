5b3e03fe241048c5845b8920662aaeaa13615fc5
ebbieaden <47478940+ebbieaden@users.noreply.github.com>
2022-11-23 22:26:18 +0100

    my nft project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xCeAC7a0F1b308cd1196b5C5Eb9CBAB5d5eDA6Ff7"
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
