02d8fca242140bd5f5855c7cc5b248cc302f2bbf
ebbieaden <47478940+ebbieaden@users.noreply.github.com>
2022-11-28 18:10:16 +0100

    updated

---
+++ my-app/pages/api/[tokenId].js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+    // get the tokenId from the query params
+    const tokenId = req.query.tokenId;
+    // as all the images are uploaded on github, we cann extract the images from github directly\
+    const image_url = "https://raw.githubusercontent.com/ebbieaden/NFT-Collection/my-app/public/cryptodevs";
+    // the api is sending back metadata for a crypto dev
+    // to make our collection compatibnle with Opensea, we need to follow some Metadata standards
+    // when sending back the response from the api
+    // More info can be found here: https://docs.opensea.io/docs/metadata-standards
+    res.status(200).json({
+        name: "Crypto Dev #" + tokenId,
+        description: "Crypto Dev is a collection of developers in Crypto",
+        image: image_url + tokenId + ".svg",
+    });
+}
